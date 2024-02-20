e264202f3ae0b577ab426c9e842283c0af77d4ff
fengzhouxuan <fengzhouxuan@qq.com>
2024-02-20 10:29:40 +0800

    fix imageloader

--- assets/Framework/Script/Base/AutoReleaseSprite.ts
+++ assets/Framework/Script/Base/AutoReleaseSprite.ts
@@ -1,93 +1,96 @@
-import { _decorator, AssetManager, assetManager, Sprite, SpriteFrame } from 'cc';
-import { StringUtils } from '../Utils/StringUtils';
+import { _decorator, AssetManager, assetManager, Sprite, SpriteFrame, Texture2D } from 'cc';
 import { ImageLoader } from '../ResourceComponent/ImageLoader';
-const { ccclass } = _decorator;
+import { StringUtils } from '../Utils/StringUtils';
+const { ccclass, property } = _decorator;
 
 @ccclass('AutoReleaseSprite')
 export class AutoReleaseSprite extends Sprite {
-  private _curLoadingUrl = "";
+    private _curLoadingUrl = "";
 
-  private _lastSpf: SpriteFrame;
-  private _curLoadedSpf: SpriteFrame;
-  onLoad(): void {
-    super.onLoad();
-  }
+    private _lastSpf: SpriteFrame;
+    private _originalSpf:SpriteFrame;
+    private _curLoadedSpf:SpriteFrame;
+    onLoad(): void {
+        super.onLoad();
+        this._originalSpf = this.spriteFrame;
+    }
 
-  load(bundleName: string, assetName: string) {
-    let self = this;
-    let loadingAssetSerialName = `${bundleName}+${assetName}`;
-    this._curLoadingUrl = loadingAssetSerialName;
-    assetManager.loadBundle(bundleName, (err: Error, Bundle: AssetManager.Bundle) => {
-      if (err) {
-        return;
-      }
-      Bundle.load(assetName, (err: Error, spf: SpriteFrame) => {
-        if (err) {
-          return;
-        }
+    load(bundleName: string, assetName: string) {
+        let self = this;
         let loadingAssetSerialName = `${bundleName}+${assetName}`;
-        if (!this || !this.isValid || self._curLoadingUrl != loadingAssetSerialName) {
-          spf.addRef();
-          spf.decRef();
-          return;
-        }
-        if (this._lastSpf) {
-          if (ImageLoader.isRemoteAsset(this._lastSpf)) {
-            ImageLoader.release(this._lastSpf);
-          } else {
-            this._lastSpf.decRef();
-          }
-        }
-        spf.addRef();
-        self.spriteFrame = spf;
-        self._curLoadedSpf = spf;
-        this._lastSpf = spf;
+        this._curLoadingUrl = loadingAssetSerialName;
+        assetManager.loadBundle(bundleName, (err: Error, Bundle: AssetManager.Bundle) => {
+            if (err) {
+                return;
+            }
+            Bundle.load(assetName, (err: Error, spf: SpriteFrame) => {
+                if (err) {
+                    return;
+                }
+                let loadingAssetSerialName = `${bundleName}+${assetName}`;
+                if (!this || !this.isValid || self._curLoadingUrl != loadingAssetSerialName) {
+                    spf.addRef();
+                    spf.decRef();
+                    return;
+                }
+                if (this._lastSpf) {
+                    if (ImageLoader.isRemoteAsset(this._lastSpf)) {
+                        ImageLoader.release(this._lastSpf);
+                    } else {
+                        this._lastSpf.decRef();
+                    }
+                }
+                spf.addRef();
+                self.spriteFrame = spf;
+                self._curLoadedSpf = spf;
+                this._lastSpf = spf;
 
-      });
-    });
-  }
-
-  loadRemote(url: string) {
-    if (StringUtils.IsNullOrEmpty(url)) {
-      return;
+            });
+        });
     }
-    let self = this;
-    this._curLoadingUrl = url;
-    ImageLoader.loadRemote(url, ".png", (spriteFrame: SpriteFrame) => {
-      if (!this || !this.isValid || self._curLoadingUrl != url) {
-        ImageLoader.release(spriteFrame);
-        return;
-      }
-      if (this._lastSpf) {
-        if (ImageLoader.isRemoteAsset(this._lastSpf)) {
-          ImageLoader.release(this._lastSpf);
-        } else {
-          this._lastSpf.decRef();
+
+    loadRemote(url: string) {
+        if(StringUtils.IsNullOrEmpty(url)){
+            return;
         }
-      }
-      self.spriteFrame = spriteFrame;
-      self._curLoadedSpf = spriteFrame;
-      this._lastSpf = spriteFrame;
-    });
-  }
-
-  release() {
-    if (!this._curLoadedSpf) {
-      return;
+        let self = this;
+        this._curLoadingUrl = url;
+        ImageLoader.loadRemote(url, ".png", (spriteFrame: SpriteFrame) => {
+            if (!this || !this.isValid || self._curLoadingUrl != url) {
+                ImageLoader.release(spriteFrame);
+                return;
+            }
+            if (this._lastSpf) {
+                if (ImageLoader.isRemoteAsset(this._lastSpf)) {
+                    ImageLoader.release(this._lastSpf);
+                } else {
+                    this._lastSpf.decRef();
+                }
+            }
+            self.spriteFrame = spriteFrame;
+            self._curLoadedSpf = spriteFrame;
+            this._lastSpf = spriteFrame;
+        });
     }
-    // let spf =this.spriteFrame;
-    if (ImageLoader.isRemoteAsset(this._curLoadedSpf)) {
-      ImageLoader.release(this._curLoadedSpf);
-    } else {
-      this._curLoadedSpf.decRef();
-    }
-    // this.spriteFrame = this._originalSpf;
-    this._lastSpf = null;
-  }
 
-  onDestroy(): void {
-    // this.spriteFrame  = this._originalSpf;
-    super.onDestroy();
-    this.release();
-  }
+    release() {
+        if(!this._curLoadedSpf){
+            return;
+        }
+        // let spf =this.spriteFrame;
+        if(ImageLoader.isRemoteAsset(this._curLoadedSpf)){
+            ImageLoader.release(this._curLoadedSpf);
+        }else{
+            this._curLoadedSpf.decRef();
+        }
+        // this.spriteFrame = this._originalSpf;
+        this._lastSpf=null;
+    }
+
+    onDestroy(): void {
+        // this.spriteFrame  = this._originalSpf;
+        super.onDestroy();
+        this.release();
+    }
 }
+
--- assets/Framework/Script/ResourceComponent/ImageLoader.ts
+++ assets/Framework/Script/ResourceComponent/ImageLoader.ts
@@ -1,74 +1,77 @@
 import { ImageAsset, SpriteFrame, Texture2D, assetManager } from "cc";
 
-export class ImageLoader {
-  private static _remoteAssets: Map<string, SpriteFrame> = new Map<string, SpriteFrame>;
+export class ImageLoader{
+    private static _remoteAssets:Map<SpriteFrame,string>=new Map<SpriteFrame,string>;
+    public static loadRemote(url:string,ext:string,complete:(spf:SpriteFrame)=>void){
+        let cachedImage = assetManager.assets.get(url);
+        // console.log("缓存：",cachedImage);
+        if(cachedImage && cachedImage.isValid){
+            //查找sp
+            let spf = this.getSpf(url);
+            if(spf && spf.isValid){
+                spf.addRef();
+                setTimeout(() => {
+                    complete && complete(spf);
+                }, 1);
+                return;
+            }
+        }
+        assetManager.loadRemote(url,{ext:ext},(err,asset)=>{
+            if(err){
+                return;
+            }
+            const img =asset as ImageAsset;
+            let spf = new SpriteFrame();
+            const texture = new Texture2D();
+            texture.image = img;
+            spf.texture = texture;
+            texture.addRef();
+            img.addRef();
+            this._remoteAssets.set(spf,img.uuid);
+            spf.addRef();
+            complete && complete(spf);
+        });
+    }
 
-  public static loadRemote(url: string, ext: string, complete: (spf: SpriteFrame) => void) {
-    let cachedImage = assetManager.assets.get(url);
-    // console.log("缓存：",cachedImage);
-    if (cachedImage && cachedImage.isValid) {
-      //查找sp
-      let spf = this._remoteAssets.get(cachedImage.uuid);
-      if (!spf && spf.isValid) {
-        spf.addRef();
-        setTimeout(() => {
-          complete && complete(spf);
-        }, 1);
-        return;
-      }
+    public static release(spf:SpriteFrame){
+       let remote = this.isRemoteAsset(spf);
+       if(!remote){
+            return;
+       }
+        spf.decRef();
+        if(spf.refCount<=0){
+            let texture = spf.texture as Texture2D;
+            // 如果已加入动态合图，必须取原始的Texture2D
+            if (spf.packable) {
+                texture = spf.original?._texture as Texture2D;
+            }
+            if (texture) {
+                texture.decRef();
+                if(texture.refCount<=0){
+                    texture.image?.decRef();
+                }
+            }
+            this._remoteAssets.delete(spf);
+            spf.destroy();
+        }
     }
-    assetManager.loadRemote(url, { ext: ext }, (err, asset) => {
-      if (err) {
-        return;
-      }
-      const img = asset as ImageAsset;
-      let spf = new SpriteFrame();
-      const texture = new Texture2D();
-      texture.image = img;
-      spf.texture = texture;
-      texture.addRef();
-      img.addRef();
-      this._remoteAssets.set(img.uuid, spf);
-      // let spf = this._remoteAssets.get(img.uuid);
-      // if(!spf || !spf.isValid || !spf.texture.isValid){
-      // }
-      spf.addRef();
-      complete && complete(spf);
-    });
-  }
 
-  public static release(spf: SpriteFrame) {
-    for (const [key, value] of this._remoteAssets) {
-      if (value != spf) {
-        return;
-      }
+    public static isRemoteAsset(spf:SpriteFrame){
+        for (const [key, value] of this._remoteAssets) {
+            if(key==spf){
+                return true;
+            }
+        }
+        return false;
     }
-    spf.decRef(false);
-    if (spf.refCount <= 0) {
-      let texture = spf.texture as Texture2D;
-      // 如果已加入动态合图，必须取原始的Texture2D
-      if (spf.packable) {
-        texture = spf.original?._texture as Texture2D;
-      }
-      if (texture) {
-        texture.decRef();
-        texture.image?.decRef();
-        // texture.image?.destroy();
-        // texture.destroy();
-      }
-      this._remoteAssets.delete(texture.image.uuid);
-      spf.destroy();
-    }
-  }
 
-  public static isRemoteAsset(spf: SpriteFrame) {
-    for (const [key, value] of this._remoteAssets) {
-      if (value == spf) {
-        return true;
-      }
+    private static getSpf(url:string):SpriteFrame{
+        for (const [key, value] of this._remoteAssets) {
+            if(value==url){
+                return key;
+            }
+        }
     }
-    return false;
-  }
 }
 
 globalThis["MLImageLoader"] = ImageLoader;
