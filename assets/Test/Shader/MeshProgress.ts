99d5e363b726e420b41b2888cf963d05da62b303
fengzhouxuan <fengzhouxuan@qq.com>
2024-04-28 18:48:11 +0800

    设置进度脚本

--- assets/Test/Shader/MeshProgress.ts
+++ assets/Test/Shader/MeshProgress.ts
@@ -1,5 +1,4 @@
 import { Component, MeshRenderer, _decorator} from 'cc';
-import { MathUtils } from '../../Framework/Script/Utils/MathUtils';
 const { ccclass,property } = _decorator;
 
 @ccclass('MeshProgress')
@@ -13,7 +12,6 @@ export class MeshProgress extends Component{
         this._value = value;
         if(this._mesh){
             let progress:number = this._value / 100;
-            // this._mesh.material.setProperty('progress',progress);
             this._mesh.setInstancedAttribute('instance_progress',[progress]);
         }
     }
@@ -22,9 +20,6 @@ export class MeshProgress extends Component{
     private _mesh:MeshRenderer = null;
     protected onLoad(): void {
         this._mesh = this.getComponent(MeshRenderer);
-    }
-    protected start(): void {
-        this.value = MathUtils.getRandomBetween(40,100,false);        
     }
 }
 
